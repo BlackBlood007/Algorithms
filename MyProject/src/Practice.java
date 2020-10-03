ab3b38436eecd69459202ff268773e902c1a9880
Satyam Sanghi <satyamsanghi24@gmail.com>
2020-10-04 02:33:56 +0530

    Revert "removing all"
    This reverts commit caf39e53ad9961671f490e7f3847cde8bb7f4073.

--- MyProject/src/Practice.java
+++ MyProject/src/Practice.java
@@ -4,7 +4,10 @@ public class Practice {
 	public static void main(String[] args) {
 		
 
-	System.out.println("one");
+		System.out.println("hello from user 24");
+		System.out.println("hello");
+		//abc
+		//def
 
 
 
