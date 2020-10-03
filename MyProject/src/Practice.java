caf39e53ad9961671f490e7f3847cde8bb7f4073
Satyam Sanghi <satyamsanghi24@gmail.com>
2020-10-04 02:33:23 +0530

    removing all

--- MyProject/src/Practice.java
+++ MyProject/src/Practice.java
@@ -4,10 +4,7 @@ public class Practice {
 	public static void main(String[] args) {
 		
 
-		System.out.println("hello from user 24");
-		System.out.println("hello");
-		//abc
-		//def
+	System.out.println("one");
 
 
 
